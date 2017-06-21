bca37327c2a48ce65bc7518cd58fa5b14e46b99c
Akul Joshi <ajoshi@akuls-macbook-air.local>
2017-06-21 10:17:40 -0500

    Initial Build of the BreakOut application.

--- BreakOut/GameScene.swift
+++ BreakOut/GameScene.swift
@@ -9,81 +9,151 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+// SKPhysicsContactDelegate - Add to use contact physics
+class GameScene: SKScene, SKPhysicsContactDelegate
+{
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var ball: SKSpriteNode!
+    var paddle: SKSpriteNode!
+    var brick: SKSpriteNode!
+    var loseZone: SKSpriteNode!
     
-    override func didMove(to view: SKView) {
+    override func didMove(to view: SKView)
+    {
+        physicsWorld.contactDelegate = self
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)   // makes edge of the view part of the physics
+        createBackground()
+        createBall()
+        createPaddle()
+        createBrick()
+        createLoseZone()
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        // this will start the ball movement
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        if ball.physicsBody?.isDynamic == false
+        {
+            ball.physicsBody?.isDynamic = true
+            ball.physicsBody?.applyImpulse(CGVector(dx: 5, dy: -5))
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        for touch in touches
+        {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x
+        }
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        for touch in touches
+        {
+            let location = touch.location(in: self)
+            paddle.position.x = location.x
+        }
+    }
+
+    func didBegin(_ contact: SKPhysicsContact)
+    {
+        if contact.bodyA.node?.name == "brick" || contact.bodyB.node?.name == "brick"
+        {
+            print("Brick Hit!")
+            brick.removeFromParent()
+        }
+        if contact.bodyA.node?.name == "lose zone" || contact.bodyB.node?.name == "lose zone"
+        {
+            print("You Lose a Ball")
+        }
+        
+    }
+    
+    
+    
+    func createBackground()
+    {
+        let stars = SKTexture(imageNamed: "stars")
+        
+        for i in 0...1  // creates two stars backgrounds for seamless transition
+        {
+            let starsBackground = SKSpriteNode(texture: stars)
+            starsBackground.zPosition = -1  // sets stacking order (lowest numbers in the very back; below everything)
+            starsBackground.anchorPoint = CGPoint(x: 0.5, y: 0.5)   // anchors the image 
+            starsBackground.position = CGPoint(x: 0, y: (starsBackground.size.height * CGFloat(i) - CGFloat(1 * i)))
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            addChild(starsBackground)
+            
+            let moveDown = SKAction.moveBy(x: 0, y: -starsBackground.size.height, duration: 20)
+            let reset = SKAction.moveBy(x: 0, y: starsBackground.size.height, duration: 0)
+            let moveLoop = SKAction.sequence([moveDown, reset])
+            let loop = SKAction.repeatForever(moveLoop)
+            
+            starsBackground.run(loop)
         }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+    func createBall()
+    {
+        let ballDiameter = frame.width / 20
+        ball = SKSpriteNode(color: UIColor.blue, size: CGSize(width: ballDiameter, height: ballDiameter))
+        ball.position = CGPoint(x: frame.midX, y: frame.midY)
+        ball.name = "ball"
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        ball.physicsBody = SKPhysicsBody(rectangleOf: ball.size)   // applies SKPhysicsBody to ball (invisible rectangle around ball)
+        
+        ball.physicsBody?.isDynamic = false     // ignores all forces and impulses
+        ball.physicsBody?.usesPreciseCollisionDetection = true
+        ball.physicsBody?.allowsRotation = false
+        ball.physicsBody?.friction = 0
+        ball.physicsBody?.affectedByGravity = false
+        ball.physicsBody?.restitution = 1
+        ball.physicsBody?.angularDamping = 0
+        ball.physicsBody?.linearDamping = 0
+        ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
+        
+        addChild(ball)
+        
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    func createPaddle()
+    {
+        paddle = SKSpriteNode(color: UIColor.white, size: CGSize(width: frame.width / 4, height: frame.height / 25))
+        paddle.position = CGPoint(x: frame.midX, y: frame.minY + 125)
+        paddle.name = "paddle"
+        
+        paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.size)
+        
+        paddle.physicsBody?.isDynamic = false
+        
+        addChild(paddle)
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func createBrick()
+    {
+        brick = SKSpriteNode(color: UIColor.green, size: CGSize(width: frame.width / 6, height: frame.height / 25))
+        brick.position = CGPoint(x: frame.midX, y: frame.maxY - 30)
+        brick.name = "brick"
+        
+        brick.physicsBody = SKPhysicsBody(rectangleOf: brick.size)
+        
+        brick.physicsBody?.isDynamic = false
+        
+        addChild(brick)
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    func createLoseZone()
+    {
+        loseZone = SKSpriteNode(color: UIColor.clear, size: CGSize(width: frame.width, height: 50))
+        loseZone.position = CGPoint(x: frame.midX, y: frame.minY + 25)
+        loseZone.name = "lose zone"
+        
+        loseZone.physicsBody = SKPhysicsBody(rectangleOf: loseZone.size)
+        
+        loseZone.physicsBody?.isDynamic = false
+        
+        addChild(loseZone)
     }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-    }
+
 }
